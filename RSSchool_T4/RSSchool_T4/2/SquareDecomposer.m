d2aa2f858665d42d1a125cfdb083f4b94abdff5b
iMintDiamond <i.mint.diamond@gmail.com>
2020-04-12 19:13:40 +0300

    feat: task2 solving

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,24 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    long goal = 0;
+    NSMutableArray *result = [[NSMutableArray alloc] initWithArray:@[number]];
+    while ([result count]) {
+        long current = [[result lastObject] longValue];
+        [result removeLastObject];
+        goal += current * current;
+        for (long i = current - 1; i >= 0; i--) {
+            if ((goal - i*i >= 0)) {
+                goal -= i*i;
+                [result addObject:[[NSNumber alloc] initWithLong:i]];
+                if (goal == 0) {
+                    NSSortDescriptor *lowestToHighest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
+                    [result sortUsingDescriptors:[NSArray arrayWithObject:lowestToHighest]];
+                    return result;
+                }
+            }
+        }
+    }
+    return nil;
 }
 @end
