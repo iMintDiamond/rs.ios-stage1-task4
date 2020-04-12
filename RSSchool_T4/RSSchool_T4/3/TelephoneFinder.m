2094ac2a8db4b70c03b9d5a30f0cb8a80d65ca7e
iMintDiamond <i.mint.diamond@gmail.com>
2020-04-12 19:13:16 +0300

    feat: task3 solving

--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -2,7 +2,36 @@
 
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+    NSDictionary *neigbors = @{
+        @"1" : @[@"2", @"4"],
+        @"2" : @[@"1", @"3", @"5"],
+        @"3" : @[@"2", @"6"],
+        @"4" : @[@"1", @"5", @"7"],
+        @"5" : @[@"2", @"4", @"6", @"8"],
+        @"6" : @[@"3", @"5", @"9"],
+        @"7" : @[@"4", @"8"],
+        @"8" : @[@"5", @"7", @"9", @"0"],
+        @"9" : @[@"6", @"8"],
+        @"0" : @[@"8"]
+    };
+    
+    NSMutableArray *result = [NSMutableArray new];
+    
+    for (int i = 0; i < [number length]; i++)
+    {
+        NSMutableString *numberCopy = [[NSMutableString alloc] initWithString:number];
+        NSString *numeral = [[NSString alloc] initWithFormat:@"%c", [number characterAtIndex:i]];
+        NSArray *numeralNeigbors = [neigbors valueForKey:numeral];
+        if ([numeralNeigbors count] == 0) {
+            return nil;
+        }
+        for (NSString *numeralNeigbor in numeralNeigbors) {
+            [numberCopy replaceCharactersInRange:NSMakeRange(i, 1) withString:numeralNeigbor];
+            NSString *newNumber = [[NSString alloc] initWithString:numberCopy];
+            [result addObject:newNumber];
+        }
+    }
+    
+    return result;
 }
 @end
